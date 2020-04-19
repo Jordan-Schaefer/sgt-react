e39da51d6561b407ec57590fae1a7b55fde4e374
Jordan Schaefer <jschaef99@gmail.com>
2020-04-19 11:06:04 -0700

    Bootstrap header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(props) {
   const title = props.text;
   return (
-    <div className='title'>
+    <div className='d-flex justify-content-between'>
       <header className='title'>{title}</header>
       <div>
         <h1>Average Grade <span className="badge badge-secondary">{props.average}</span></h1>
